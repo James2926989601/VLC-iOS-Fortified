5389f4a21113fe94136a57326121726688433211
James2926989601 <zhanty06@hotmail.com>
2026-09-12 08:51:58 +0800

    Add files via upload

---
+++ VLCThumbnailsCache.h
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ * VLCThumbnailsCache.h
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2013-2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Gleb Pinigin <gpinigin # gmail.com>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
+ *          Carola Nitz <caro # videolan.org>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+@interface VLCThumbnailsCache : NSObject
+
+// Preserves source pixel dimensions.
++ (nullable UIImage *)thumbnailForURL:(nullable NSURL *)url;
+// Reserved for non-artwork thumbnails that explicitly permit downsampling.
++ (nullable UIImage *)thumbnailForURL:(nullable NSURL *)url maxPixelSize:(CGFloat)maxPixelSize;
++ (void)invalidateThumbnailForURL:(nullable NSURL *)url;
+
+@end
---
+++ VLCThumbnailsCache.m
@@ -0,0 +1,193 @@
+/*****************************************************************************
+ * VLCThumbnailsCache.m
+ * VLC for iOS
+ *****************************************************************************
+ * Copyright (c) 2013-2026 VideoLAN. All rights reserved.
+ * $Id$
+ *
+ * Authors: Gleb Pinigin <gpinigin # gmail.com>
+ *          Felix Paul Kühne <fkuehne # videolan.org>
+ *          Carola Nitz <caro # videolan.org>
+ *          Tobias Conradi <videolan # tobias-conradi.de>
+ *
+ * Refer to the COPYING file of the official project for license.
+ *****************************************************************************/
+
+#import "VLCThumbnailsCache.h"
+
+#import <ImageIO/ImageIO.h>
+
+@interface VLCThumbnailsCache() {
+    NSInteger MaxCacheSize;
+    NSCache *_thumbnailCache;
+    NSInteger _currentDeviceIdiom;
+    NSMutableSet<NSNumber *> *_knownMaxPixelSizes;
+    NSLock *_knownMaxPixelSizesLock;
+}
+@end
+
+@implementation VLCThumbnailsCache
+
+#define MAX_CACHE_SIZE_IPHONE (16 * 1024 * 1024)
+#define MAX_CACHE_SIZE_IPAD   (24 * 1024 * 1024)
+#define MAX_CACHE_SIZE_WATCH  (8 * 1024 * 1024)
+#define MAX_CACHE_SIZE_tvOS   (64 * 1024 * 1024)
+#define VLC_ORIGINAL_PIXEL_SIZE 0.f
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        MaxCacheSize = 0;
+#if TARGET_OS_WATCH
+        // On watchOS, we don't have a `UIDevice`, so we set a fixed cache size
+        MaxCacheSize = MAX_CACHE_SIZE_WATCH;
+#else
+        _currentDeviceIdiom = [[UIDevice currentDevice] userInterfaceIdiom];
+        switch (_currentDeviceIdiom) {
+            case UIUserInterfaceIdiomPad:
+                MaxCacheSize = MAX_CACHE_SIZE_IPAD;
+                break;
+            case UIUserInterfaceIdiomPhone:
+                MaxCacheSize = MAX_CACHE_SIZE_IPHONE;
+                break;
+            case UIUserInterfaceIdiomTV:
+                MaxCacheSize = MAX_CACHE_SIZE_tvOS;
+                break;
+            default:
+                MaxCacheSize = MAX_CACHE_SIZE_WATCH;
+                break;
+        }
+#endif
+        _thumbnailCache = [[NSCache alloc] init];
+        [_thumbnailCache setTotalCostLimit: MaxCacheSize];
+        _knownMaxPixelSizes = [NSMutableSet set];
+        _knownMaxPixelSizesLock = [[NSLock alloc] init];
+    }
+    return self;
+}
+
++ (instancetype)sharedThumbnailCache
+{
+    static dispatch_once_t onceToken;
+    static VLCThumbnailsCache *sharedThumbnailCache;
+    dispatch_once(&onceToken, ^{
+        sharedThumbnailCache = [[VLCThumbnailsCache alloc] init];
+    });
+
+    return sharedThumbnailCache;
+}
+
++ (UIImage *)thumbnailForURL:(NSURL *)url
+{
+    return [VLCThumbnailsCache thumbnailForURL:url maxPixelSize:VLC_ORIGINAL_PIXEL_SIZE];
+}
+
++ (UIImage *)thumbnailForURL:(NSURL *)url maxPixelSize:(CGFloat)maxPixelSize
+{
+    VLCThumbnailsCache *sharedCache = [VLCThumbnailsCache sharedThumbnailCache];
+    return [sharedCache _thumbnailForURL:url maxPixelSize:maxPixelSize];
+}
+
++ (void)invalidateThumbnailForURL:(nullable NSURL *)url
+{
+    if (!url) {
+        return;
+    }
+    VLCThumbnailsCache *sharedCache = [VLCThumbnailsCache sharedThumbnailCache];
+    [sharedCache _invalidateThumbnailForURL:url];
+}
+
+- (NSString *)cacheKeyForURL:(NSURL *)url maxPixelSize:(CGFloat)maxPixelSize
+{
+    if (maxPixelSize <= VLC_ORIGINAL_PIXEL_SIZE) {
+        return [NSString stringWithFormat:@"%@|original", url.path];
+    }
+    return [NSString stringWithFormat:@"%@|%.0f", url.path, maxPixelSize];
+}
+
+- (UIImage *)_thumbnailForURL:(NSURL *)url maxPixelSize:(CGFloat)maxPixelSize
+{
+    if (url == nil || !url.isFileURL)
+        return nil;
+
+    NSString *path = url.path;
+    if (path.length == 0)
+        return nil;
+
+    NSString *key = [self cacheKeyForURL:url maxPixelSize:maxPixelSize];
+    UIImage *theImage = [_thumbnailCache objectForKey:key];
+    if (theImage) {
+        return theImage;
+    }
+
+    theImage = [self imageAtPath:path maxPixelSize:maxPixelSize];
+    if (!theImage) {
+        return nil;
+    }
+
+    [_knownMaxPixelSizesLock lock];
+    [_knownMaxPixelSizes addObject:@(maxPixelSize)];
+    [_knownMaxPixelSizesLock unlock];
+
+    [_thumbnailCache setObject:theImage forKey:key cost:[self costForImage:theImage]];
+
+    return theImage;
+}
+
+- (UIImage *)imageAtPath:(NSString *)path maxPixelSize:(CGFloat)maxPixelSize
+{
+    if (maxPixelSize <= VLC_ORIGINAL_PIXEL_SIZE) {
+        return [UIImage imageWithContentsOfFile:path];
+    }
+
+    NSURL *url = [NSURL fileURLWithPath:path];
+    CGImageSourceRef source = CGImageSourceCreateWithURL((__bridge CFURLRef)url, NULL);
+    if (source == NULL) {
+        APLog(@"Failed to read thumbnail at path '%@'", path);
+        return nil;
+    }
+
+    NSDictionary *options = @{
+        (__bridge NSString *)kCGImageSourceCreateThumbnailFromImageAlways: @YES,
+        (__bridge NSString *)kCGImageSourceCreateThumbnailWithTransform: @YES,
+        (__bridge NSString *)kCGImageSourceShouldCacheImmediately: @YES,
+        (__bridge NSString *)kCGImageSourceThumbnailMaxPixelSize: @(maxPixelSize)
+    };
+
+    CGImageRef cgImage = CGImageSourceCreateThumbnailAtIndex(source, 0, (__bridge CFDictionaryRef)options);
+    CFRelease(source);
+
+    if (cgImage == NULL) {
+        APLog(@"Failed to decode thumbnail at path '%@'", path);
+        return nil;
+    }
+
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
+
+    return image;
+}
+
+- (NSUInteger)costForImage:(UIImage *)image
+{
+    CGImageRef cgImage = image.CGImage;
+    if (cgImage == NULL) {
+        return 0;
+    }
+    return CGImageGetBytesPerRow(cgImage) * CGImageGetHeight(cgImage);
+}
+
+- (void)_invalidateThumbnailForURL:(NSURL *)url
+{
+    [_knownMaxPixelSizesLock lock];
+    NSArray<NSNumber *> *maxPixelSizes = _knownMaxPixelSizes.allObjects;
+    [_knownMaxPixelSizesLock unlock];
+
+    for (NSNumber *maxPixelSize in maxPixelSizes) {
+        [_thumbnailCache removeObjectForKey:[self cacheKeyForURL:url
+                                                    maxPixelSize:maxPixelSize.doubleValue]];
+    }
+}
+
+@end
